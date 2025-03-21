089f4eda50de282737cabe8ea776964085267ad5
rioadityapratama <rioaditya444@gmail.com>
2025-03-21 22:18:35 +0800

    menambah method POST /users

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ type User struct {
     UpdatedAt time.Time `gorm:"column:updatedAt"`
 }
 
+
+
 func main() {
     dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -27,12 +29,25 @@ func main() {
     }
 
     router := gin.Default()
-
+//Get
     router.GET("/users", func(c *gin.Context) {
         var users []User
         db.Find(&users)
         c.JSON(http.StatusOK, gin.H{"data": users})
     })
+//POST
+	router.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+		db.Create(&user)
+		c.JSON(http.StatusCreated, gin.H{"data": user})
+	})
 
+	//
     router.Run(":3000")
 }
